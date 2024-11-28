993cb245dc6874441b8e70a9eae1fc42d4051b1e
MrBus1ness <148568744+MrBus1ness@users.noreply.github.com>
2024-11-27 21:52:55 -0500

    updating database selection

--- index1.php
+++ index1.php
@@ -5,7 +5,7 @@ Final Project Index
 
 <?php
 $host = 'localhost'; 
-$dbname = 'test'; 
+$dbname = 'card_shop'; 
 $user = 'hunter'; 
 $pass = 'hunter';
 $charset = 'utf8mb4';
@@ -27,7 +27,7 @@ try {
 $search_results = null;
 if (isset($_GET['search']) && !empty($_GET['search'])) {
     $search_term = '%' . $_GET['search'] . '%';
-    $search_sql = 'SELECT student_id, student_name, class_grade FROM grades WHERE student_id LIKE :search';
+    $search_sql = 'SELECT student_id, student_name, class_grade FROM lea WHERE student_id LIKE :search';
     $search_stmt = $pdo->prepare($search_sql);
     $search_stmt->execute(['search' => $search_term]);
     $search_results = $search_stmt->fetchAll();
@@ -40,21 +40,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $student_name = htmlspecialchars($_POST['student_name']);
         $class_grade = htmlspecialchars($_POST['class_grade']);
         
-        $insert_sql = 'INSERT INTO grades (student_id, student_name, class_grade) VALUES (:student_id, :student_name, :class_grade)';
+        $insert_sql = 'INSERT INTO lea (student_id, student_name, class_grade) VALUES (:student_id, :student_name, :class_grade)';
         $stmt_insert = $pdo->prepare($insert_sql);
         $stmt_insert->execute(['student_id' => $student_id, 'student_name' => $student_name, 'class_grade' => $class_grade]);
     } elseif (isset($_POST['delete_id'])) {
         // Delete an entry
         $delete_id = (int) $_POST['delete_id'];
         
-        $delete_sql = 'DELETE FROM grades WHERE student_id = :student_id';
+        $delete_sql = 'DELETE FROM lea WHERE student_id = :student_id';
         $stmt_delete = $pdo->prepare($delete_sql);
         $stmt_delete->execute(['student_id' => $delete_id]);
     }
 }
 
 // get everything for main table
-$sql = 'SELECT student_id, student_name, class_grade FROM grades';
+$sql = 'SELECT student_id, student_name, class_grade FROM lea';
 $stmt = $pdo->query($sql);
 ?>
 
@@ -130,7 +130,7 @@ $stmt = $pdo->query($sql);
         <input type="submit" value="Add Entry">
     </form>
 
-    <h1>Data Listing from Table 'grades'</h1>
+    <h1>Data Listing from Table 'lea'</h1>
     <table class="half-width-left-align">
         <thead>
             <tr>
