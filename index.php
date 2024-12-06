6af8e0fc4ba2ec118115e06cf7dd54609070c409
MrBus1ness <148568744+MrBus1ness@users.noreply.github.com>
2024-12-06 13:32:23 -0500

    big progress actually

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MTG Deck Builder - Draftsman</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
